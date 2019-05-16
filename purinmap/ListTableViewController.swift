63c7d2c70c9454b15bcbe1c870164099bb034108
Koutaroutarou <kw_extaro24karats@ezweb.ne.jp>
2019-05-16 16:32:31 +0900

    テーブルビュー削除可能成功

--- purinmap/ListTableViewController.swift
+++ purinmap/ListTableViewController.swift
@@ -58,7 +58,25 @@ class ListTableViewController: UITableViewController {
         return cell
     }
     
-
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        
+        return true
+    
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if  editingStyle == UITableViewCell.EditingStyle.delete {
+            
+            
+            try! realm.write {
+                realm.delete(self.puddingArray![indexPath.row])
+            }
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+        }
+    }
+    
+    
 
     
 
